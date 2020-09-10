58bef67069aa08c2e423ea707aa7aa6a55ae64cc
Anachu57660 <44734568+Anachu57660@users.noreply.github.com>
2020-09-10 15:01:05 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,15 @@
 import csv
 
 
-with open('AOIT_Internship_Excel.csv', 'r') as file:
+with open('Bundle_Spreadsheet.csv', 'r') as file:
     reader = csv.reader(file)
     line_count = 0
+    list = []
     for row in reader:
+        list.append(row)
         print(row)
+    print()
+    print(list)
+    print()
+    print(list[0][0])
+
